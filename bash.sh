02402866420d8e12003d1ac390acba5389e075f9
web4 <lunghugh@gmail.com>
2026-09-26 06:18:47 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,9 @@
 git clone https://github.com/web4hub/prompts.chat.git
 cd prompts.chat
 npm install && npm run setup
+kubu prompt search shader
+kubu prompt execute luxury-watch
+kubu prompt translate luxury-watch --lang fr
+kubu prompt lint prompts/
+kubu prompt embed prompts/
+kubu prompt publish
